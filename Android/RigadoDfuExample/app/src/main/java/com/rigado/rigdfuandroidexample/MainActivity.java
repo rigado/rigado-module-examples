d8e960db393aec9582665c79c26f92bce32b507d
Eric Stutzenberger <eric.stutzenberger@rigado.com>
2015-01-13 12:00:21 -0800

    add start of android dfu example project

---
+++ Android/RigadoDfuExample/app/src/main/java/com/rigado/rigdfuandroidexample/MainActivity.java
@@ -0,0 +1,53 @@
+package com.rigado.rigdfuandroidexample;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class MainActivity extends Activity {
+    public final static String EXTRA_MESSAGE = "com.rigado.rigdfuandroidexample.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch(id)
+        {
+            case R.id.action_search:
+                //openSearch();
+                return true;
+            case R.id.action_settings:
+                //openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /** Called when the user clicks the send message button */
+    public void beginUpdate(View view) {
+
+    }
+}
